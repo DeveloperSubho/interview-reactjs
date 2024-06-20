7394776fe222cebb179a5c29d2af501a0a52774f
SUBHAJIT SARKAR <ssubho.srkr@gmail.com>
2024-06-20 15:18:33 +0530

    SS-200424: ToDo apps added

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,59 +1,47 @@
 import React, { useState } from "react";
-import "./App.css";
+import ToDoInput from "./ToDoInput";
+import ToDoList from "./ToDoList";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const handleChange = (e) => {
-    setInputText(e.target.value);
+  const addTodo = (todo) => {
+    setTodos([...todos, { text: todo, completed: false }]);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (inputText.trim() !== "") {
-      setTodos([...todos, { text: inputText, completed: false }]);
-      setInputText("");
-    }
-  };
-
-  const toggleCompleted = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos[index].completed = !updatedTodos[index].completed;
-    setTodos(updatedTodos);
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
   };
 
   const removeTodo = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
+    /**
+     * Callback function (_, i) => i !== index:
+     * The filter method takes a callback function as an argument. This callback function is executed for each element in the array.
+     * The callback function takes two parameters:
+     * * The first parameter (typically element or _ in this case) represents the current element being processed in the array.
+     * * The second parameter (i) represents the index of the current element being processed in the array.
+     * In this case, the underscore _ is used as a placeholder for the first parameter since it is not needed in the function body.
+     */
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
   };
 
   return (
-    <div className="App">
-      <h1>ToDo App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter a todo item"
-          value={inputText}
-          onChange={handleChange}
-        />
-        <button type="submit">Add Item</button>
-      </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li
-            key={index}
-            className={todo.completed ? "completed" : ""}
-            onClick={() => toggleCompleted(index)}
-          >
-            {todo.text}
-            <button onClick={() => removeTodo(index)}>X</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <Typography variant="h2" className="text-center my-4">
+        ToDo App
+      </Typography>
+      <ToDoInput addTodo={addTodo} />
+      <ToDoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
+      />
+    </Container>
   );
 }
 
